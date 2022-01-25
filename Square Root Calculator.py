478ffb60a413a49bf93e0ec84967ccafe6c60398
DaksheshSharma <98314906+DaksheshSharma@users.noreply.github.com>
2022-01-25 10:15:33 +0530

    Add files via upload

---
+++ Square Root Calculator.py
@@ -0,0 +1,6 @@
+#Made by Dakshesh Sharma
+#https://github.com/DaksheshSharma/Python-Projects
+for i in range(1,501):    
+    no = float(input("Enter a no : "))
+    a = no ** 0.5
+    print(f"The square root of {no} is {a}.")
---
+++ Trigonometry Calculator.py
@@ -0,0 +1,8 @@
+#Made by Dakshesh Sharma
+#https://github.com/DaksheshSharma/Python-Projects
+import math
+for i in range(1, 501):
+    no = float(input("Enter a no : "))
+    print(f"The value of cos {no} is {math.cos (no)}")
+    print(f"The value of sin {no} is {math.sin (no)}")
+    print(f"The value of tan {no} is {math.tan (no)}")  
